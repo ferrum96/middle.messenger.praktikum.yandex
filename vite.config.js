4979998c43d07910d2ec84c6ee4bcdccac0b56ab
Константин <51968997+ferrum96@users.noreply.github.com>
2024-03-01 21:10:40 +0300

    Настроен стартовый шаблон проекта

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,29 @@
-// import { defineConfig } from 'vite';
-//
-// export default defineConfig({
-//     build: {
-//         rollupOptions: {
-//             input: {
-//                 main: './src/main.js' // Путь к вашему основному файлу JavaScript
-//             }
-//         }
-//     },
-//     server: {
-//         // Путь к вашему HTML-файлу
-//         // По умолчанию Vite будет искать файл в корне проекта с именем index.html
-//         // Если ваш файл имеет другое имя или находится в другом месте, укажите его здесь
-//         fs: {
-//             strict: false,
-//             allow: ['public'] // Разрешить доступ к родительским каталогам
-//         }
-//     }
-// });
-
-import {resolve} from 'path';
-import {defineConfig} from 'vite';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
 
 export default defineConfig({
-    root: resolve(__dirname, 'src/'),
-    build: {
-        rollupOptions: {
-            input: {
-                main: resolve(__dirname, 'src/index.html'),
-            },
-        },
-        outDir: resolve(__dirname, 'dist/'),
-    },
+	server: {
+		port: '3000',
+		strictPort: true,
+	},
+	root: resolve(__dirname, 'src/'),
+	build: {
+		port: '3000',
+		strictPort: true,
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, 'src/index.html'),
+			},
+		},
+		outDir: resolve(__dirname, 'dist/'),
+	},
+	plugins: [
+		checker({
+			typescript: true,
+			// eslint: {
+			// 	lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+			// },
+		}),
+	],
 })
