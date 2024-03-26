9ff95ccbd6e5c4cee0ec34619d6937e76443e5d2
Константин <51968997+ferrum96@users.noreply.github.com>
2024-03-26 16:11:11 +0300

    Воспомогательные модули

---
+++ src/utils/renderDOM.ts
@@ -0,0 +1,8 @@
+import Block from './Block';
+
+export function render(query: string, block: Block) {
+  const root = document.querySelector(query);
+  root.appendChild(block.getContent());
+  block.dispatchComponentDidMount();
+  return root;
+}
